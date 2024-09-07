9693b5c67943cc51af95caa2858f30070046da96
H2605 <hhoaduc@yahoo.com>
2024-09-07 10:03:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,4 +103,4 @@ try:
     st.text("Unter Berücksichtigung der Faktoren Baujahr, Sanierung und Zustand wurde ein Mietpreis von "+mietpreis_pro_qm+"€ pro Quadratmeter errechnet.\nDie monatliche Kaltmiete für die Wohnung sollte deswegen "+mietpreis_gesamt+"€ betragen.")
     st.text("Mit "+people+" Personen im Haushalt ergibt das eine Warmmiete von "+warmmiete+"€ (Strom und Heizkosten nicht inbegriffen).")
 except:
-  st.error("Für den ausgewählen Ort ein Mietspiegel leider noch nicht verfügbar")
+  st.error("Für den ausgewählen Ort ist ein Mietspiegel leider noch nicht verfügbar")
